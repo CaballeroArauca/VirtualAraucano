602e74204aeb70feb0c931ad50b7c6c97d71f3b3
Carlos Arevalo <carlosarevalo19@hotmail.com>
2017-09-01 10:46:37 -0500

    mejora clase e instancia calificaciones

--- modelos/calificacion/calificacion.php
+++ modelos/calificacion/calificacion.php
@@ -6,18 +6,20 @@
 
 	$id_jurado = $_SESSION['id'];
 	$grupo = $_POST["nombre_grupo"];
-	$ritmo = $_POST["ritmo"] * 0.2;
-	$dificulta = $_POST["dificulta"] * 0.15;
-	$zapateo = $_POST["zapateo"] * 0.2;
-	$suma = $ritmo + $dificulta + $zapateo;
+	$_Calificacion_1 = $_POST["Calificacion_1"] * 0.25;
+	$_Calificacion_2 = $_POST["Calificacion_2"] * 0.25;
+	$_Calificacion_3 = $_POST["Calificacion_3"] * 0.25;
+	$_Calificacion_4 = $_POST["Calificacion_4"] * 0.25;
+	$suma = $_Calificacion_1 + $_Calificacion_2 + $_Calificacion_3 + $_Calificacion_4;
 	$puntaje = round( $suma, 2, PHP_ROUND_HALF_UP);
 	$calificaciones = new Calificaciones();
 
 	$calificaciones->set_jurado($id_jurado);
 	$calificaciones->set_nombreGrupo($grupo);
-	$calificaciones->set_ritmo($ritmo);
-	$calificaciones->set_dificulta($dificulta);
-	$calificaciones->set_zapateo($zapateo);
+	$calificaciones->set_Calificacion_1($_Calificacion_1);
+	$calificaciones->set_Calificacion_2($_Calificacion_2);
+	$calificaciones->set_Calificacion_3($_Calificacion_3);
+	$calificaciones->set_Calificacion_4($_Calificacion_4);
 	$calificaciones->set_puntaje($puntaje);
 	$calificaciones->get_idValidar();
 	$calificaciones->id_grupo();
