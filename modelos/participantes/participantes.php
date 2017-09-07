eb8183c133211974fd14522dfc90d34b9b001093
ricardo.garciav <ricardocrem14@hotmail.com>
2017-09-07 14:42:31 -0500

    mejoras

--- modelos/participantes/participantes.php
+++ modelos/participantes/participantes.php
@@ -23,6 +23,12 @@
 			echo "</table>";
 		break;
 
+		case 'get_Img':
+			$Img = new Participante();
+			$turnoActual = $Img->participante_actual();
+			echo "<img class='img-responsive' src='../img/participantes/".$turnoActual.".PNG'>";
+		break;
+
 		case 'get_Anterior':
 			$anterior = new Participante();
 			$turnoActual = $anterior->participante_actual();
--- sitiosWeb/js/calificaciones/puntuacion.js
+++ sitiosWeb/js/calificaciones/puntuacion.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	get_Lista();
 	setInterval(carga, 10000);
 
 	function carga(){
@@ -11,9 +12,20 @@ $(function() {
 			url:"../../modelos/participantes/participantes.php?peticion=get_Lista",
 			success:function(data) {
 				$('#lista').html(data);
+				get_Img();
+			}
+		});
+	}
+
+	function get_Img() {
+		$.ajax({
+			url:"../../modelos/participantes/participantes.php?peticion=get_Img",
+			success:function(data) {
+				$('#img').html(data);
 				get_Anterior();
 			}
 		});
+
 	}
 
 	function get_Anterior() {
